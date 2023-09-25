871797d3cc6d6131ea66cc05fe792689a5eecaa4
joacimwall <joacim.wall@tietoevry.com>
2023-09-25 20:04:09 +0200

    roi

--- Client/MySolarCells/Services/RoiService.cs
+++ Client/MySolarCells/Services/RoiService.cs
@@ -18,24 +18,29 @@ public class RoiService : IRoiService
 	{
 		RoiStats roiStats = new RoiStats();
         using var dbContext = new MscDbContext();
-        //var test  = dbContext.Energy.FromSqlRaw("select sum(PurchasedCost) as PurchasedCost_kr from Energy");
-
-        //dbContext.Energy.Where(x => x.Input).GroupBy(x => x.GoodsID).Select(x => x.Sum(y => y.Quantity));
+        var calcparms = await dbContext.EnergyCalculationParameter.FirstAsync(); 
+        //Base sums
         roiStats.TotalPurchasedCost = Convert.ToInt32( dbContext.Energy.Sum(x => x.PurchasedCost));
         roiStats.TotalPurchased = Convert.ToInt32(dbContext.Energy.Sum(x => x.Purchased));
         roiStats.TotalProductionSoldProfit = Convert.ToInt32(dbContext.Energy.Sum(x => x.ProductionSoldProfit));
         roiStats.TotalProductionSold = Convert.ToInt32(dbContext.Energy.Sum(x => x.ProductionSold));
         roiStats.TotalProductionOwnUseProfit = Convert.ToInt32(dbContext.Energy.Sum(x => x.ProductionOwnUseProfit));
         roiStats.TotalProductionOwnUse = Convert.ToInt32(dbContext.Energy.Sum(x => x.ProductionOwnUse));
+        //Calc
+        roiStats.TotalCompensationForProductionToGrid = Convert.ToInt32(roiStats.TotalProductionSold * calcparms.ProdCompensationElectricityLowload);
+        roiStats.TotalSavedTransferFeeProductionOwnUse = Convert.ToInt32(roiStats.TotalProductionOwnUse * calcparms.TransferFee);
+        roiStats.TotalSavedEnergyTaxProductionOwnUse = Convert.ToInt32(roiStats.TotalProductionOwnUse * calcparms.EnergyTax);
+        roiStats.TotalSavedEnergyTaxReductionProductionToGrid = Convert.ToInt32(roiStats.TotalProductionSold * calcparms.TaxReduction);
 
 
+        roiStats.TotalSaved = Convert.ToInt32(roiStats.TotalProductionSoldProfit + roiStats.TotalProductionOwnUseProfit + roiStats.TotalCompensationForProductionToGrid
+                            + roiStats.TotalSavedTransferFeeProductionOwnUse + roiStats.TotalSavedEnergyTaxProductionOwnUse + roiStats.TotalSavedEnergyTaxReductionProductionToGrid);
         return roiStats;
 	}
 }
 public class RoiStats
 {
     public int TotalProductionSold { get; set; } = 0;
-  
     public int TotalProductionOwnUse { get; set; } = 0;
     public int TotalPurchased { get; set; } = 0;
 
@@ -44,5 +49,14 @@ public class RoiStats
     public int TotalProductionSoldProfit { get; set; } = 0;
     public int TotalProductionOwnUseProfit { get; set; } = 0;
     public int TotalPurchasedCost { get; set; } = 0;
-    
+
+    //calc values
+    public int TotalCompensationForProductionToGrid { get; set; } = 0;
+    public int TotalSavedTransferFeeProductionOwnUse { get; set; } = 0;
+    public int TotalSavedEnergyTaxProductionOwnUse { get; set; } = 0;
+    public int TotalSavedEnergyTaxReductionProductionToGrid { get; set; } = 0;
+
+    //Total Saved
+    public int TotalSaved { get; set; } = 0;
+
 }
